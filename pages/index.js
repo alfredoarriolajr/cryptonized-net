73349ec11e3cd0c8a2353fd2001d5650d9ff3c5a
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2023-01-04 18:49:01 -0800

    mr

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
                         <form onSubmit={changeGreeting}>
                             <label>Change greeting:</label>
                             <input
-                                className='mx-4 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                                className='mr-4 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                                 autoComplete='off'
                                 id='greetingInput'
                                 placeholder='New message'
